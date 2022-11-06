b93d2d7daaf10ff5d934bc2f24b5de1aeaeeed3b
Hamza Afzal <95589034+ArceusDev@users.noreply.github.com>
2022-11-06 12:17:16 -0800

    Initial Ball and Paddle Movement done
    Ball Movement is Constant yet.
    Paddle Functionality is Complete.
    Walls and Bottom Triggered Implemented.
    Block Destroyer is yet to implement.

---
+++ Block Smasher/Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float screenWidthInUnit = 16f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = (Input.mousePosition.x / Screen.width) * screenWidthInUnit;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = paddlePos;
+
+    }
+}
